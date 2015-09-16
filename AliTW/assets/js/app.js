616f1a0ebd453b6f1bf0c87f45a004876058cc04
Ali zoghi <azouqi@gmail.com>
2015-09-15 20:45:45 -0400

    Tablet version is done

--- AliTW/assets/js/app.js
+++ AliTW/assets/js/app.js
@@ -20,6 +20,16 @@ init_jssor_slider1("slider1_container");
 //Range Slider
 var $RangeInput=$("#RangeOfYears");
 $RangeInput.val(0);
+
+//Open and close pricing plan in Tablet size screen
+var $pricingTab=$(".setting");
+$pricingTab.click(function(e) {
+	var $WindowsSize= $(document).width();
+	if($WindowsSize<991){
+		$(this).children("div").nextAll("div").toggleClass("hide");
+	}
+});
+
 //Change order in Page three
 $( document ).ready( changeOrder());
 
@@ -27,16 +37,52 @@ function changeOrder(){
 	var $WindowsSize= $(document).width();
 	var $SliderContainer=$("#slider1_container");
 	if($WindowsSize<991){
+		//Change the order of image and explanation
 		$("#pageThree p").insertBefore("#pageThree aside");
+		
+		//remove circle of pictures in Tablet size screen
 		$("#pageFive .col-sm-4").hide();
+		//show slider of pictures in Tablet size screen
+		$SliderContainer.insertAfter("#pageFive h1");
 		$SliderContainer.css("display","block");
 		init_jssor_slider1("slider1_container");
+		
+		//close pricing plan in Tablet size screen
+		$pricingTab.children("div").nextAll("div").addClass("hide");
 }else{
+	//Change the order of image and explanation
 	$("#pageThree aside").insertBefore("#pageThree p");
+	
+	//remove slider of pictures in Desktop size screen
 	$SliderContainer.css("display","none");
+	//show circle of pictures in Desktop size screen
+	$("#pageFive .col-sm-4").show();
+	
+	$pricingTab.children("div").nextAll("div").removeClass("hide");
 }
+};
+//Tablet menu will be showed in Tablet size 
+function ShowMenuTablet(){
+	var $topDistance=$(window).scrollTop();
+	var $WindowsSize= $(document).width();
+	if($topDistance>70 && $WindowsSize<991){
+		$("#TabletMenu").slideDown();
+	}
+	else{
+		$("#TabletMenu").slideUp();
+		$("#TabletMenuButton ul").hide();
+	}
 };
 $(window).resize(changeOrder);
 
+//scroll event for showing Menu in Tablet size
+$(window).scroll(ShowMenuTablet);
+//scroll event for remove list of menu in Tablet size when scrolling
+$(window).scroll(function(){
+	$("#TabletMenuButton ul").slideUp("fast");
+});
+//Add list to menu in tablet size
+$("#TabletMenuButton button").click(function() {
+    $("#TabletMenuButton ul").slideToggle("fast");
+});
 
-//remove slide show from desktop and add to tablet size
